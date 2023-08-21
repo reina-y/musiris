4a690317c7c1ff3d27d0b1bd6b801ac35e8eb9e1
reina-y <yamayama017moto@gmail.com>
2023-08-21 22:46:51 +0900

    ADD：楽譜詳細画面追加

---
+++ resources/views/show.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Musiris</title>
+    </head>
+    <body>
+        <h1 class="title">
+            {{ $post->title }}
+        </h1>
+        <div class="content">
+            <div class="content__post">
+                <p>楽譜</p>
+                <p>{{ $post->instruments }}</p>
+                <p>{{ $post->body }}</p>    
+            </div>
+        </div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>    
